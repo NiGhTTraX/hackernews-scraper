81ada9310b753ef8d82b4c495aca195e1d9559c1
Claudiu Coman <claud.coman@gmail.com>
2014-03-27 18:59:58 +0200

    Reindented code to 4 spaces #5

--- hackernews_scraper/endpoints.py
+++ hackernews_scraper/endpoints.py
@@ -2,46 +2,45 @@ import requests
 
 
 class AlgoliaEndpoint(object):
-  """Class used to call the Algolia API and parse the response."""
+    """Class used to call the Algolia API and parse the response."""
 
-  URL = "http://hn.algolia.com/api/v1/search_by_date"
+    URL = "http://hn.algolia.com/api/v1/search_by_date"
 
-  @staticmethod
-  def get(tag, since, until=None, page=0):
-    """Send a GET request to the endpoint.
+    @staticmethod
+    def get(tag, since, until=None, page=0):
+        """Send a GET request to the endpoint.
 
-    Since Algolia only returns JSON, parse it into a dict.
+        Since Algolia only returns JSON, parse it into a dict.
 
-    See http://hn.algolia.com/api for more details.
+        See http://hn.algolia.com/api for more details.
 
-    Params:
-      tag: Can be "story" or "comment".
-      since: timestamp representing how old the news should be.
+        Params:
+          tag: Can be "story" or "comment".
+          since: timestamp representing how old the news should be.
 
-    Optional params:
-      until: timestamp representing how new the news should be.
-      page: The number of the page to get.
+        Optional params:
+          until: timestamp representing how new the news should be.
+          page: The number of the page to get.
 
-    Returns:
-      A python dict representing the response.
+        Returns:
+          A python dict representing the response.
 
-    Raises:
-      requests.exceptions.RequestException.
-    """
+        Raises:
+          requests.exceptions.RequestException.
+        """
 
-    numericFilters = ["created_at_i>%d" % since]
-    if until is not None:
-      numericFilters.append("created_at_i<%d" % until)
+        numericFilters = ["created_at_i>%d" % since]
+        if until is not None:
+            numericFilters.append("created_at_i<%d" % until)
 
-    params = {
-        "numericFilters": ",".join(numericFilters),
-        "tags": tag,
-        "page": page
-    }
+        params = {
+            "numericFilters": ",".join(numericFilters),
+            "tags": tag,
+            "page": page
+        }
 
-    url = AlgoliaEndpoint.URL
-    url += "?" + "&".join(["%s=%s" % (k, v) for k, v in params.iteritems()])
-    response = requests.get(url)
+        url = AlgoliaEndpoint.URL
+        url += "?" + "&".join(["%s=%s" % (k, v) for k, v in params.iteritems()])
+        response = requests.get(url)
 
-    return response.json()
-
+        return response.json()
