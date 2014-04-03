12483f1996cabaedd39f4acdc5f85209f92dd41f
Andrei Picus <office.nightcrawler@gmail.com>
2014-04-03 16:58:44 +0300

    Import TooManyPagesException in __init__.

--- hackernews_scraper/__init__.py
+++ hackernews_scraper/__init__.py
@@ -1,3 +1,3 @@
 __version__ = '0.1'
 
-from hackernews_scraper.hnscraper import CommentScraper, StoryScraper
+from hackernews_scraper.hnscraper import CommentScraper, StoryScraper, TooManyItemsException
