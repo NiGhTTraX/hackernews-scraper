74d4b65e0bc0e4bb3c02fd1258f79c98e3cddf8a
Andrei Picus <office.nightcrawler@gmail.com>
2014-04-03 16:29:30 +0300

    Add comment scraper tests.

---
+++ tests/test_comment_scraper.py
@@ -0,0 +1,37 @@
+import httpretty
+import json
+from mock import patch
+import unittest
+
+from hackernews_scraper.endpoints import AlgoliaEndpoint
+from hackernews_scraper.hnscraper import Scraper, CommentScraper
+from .utils import BaseTest, CommentFactory
+
+
+class TestCommentScraper(BaseTest):
+    ENDPOINT_GET_PATH = "hackernews_scraper.hnscraper.Scraper.scrape"
+
+    @httpretty.activate
+    def test_correct_tag(self):
+        with patch(self.ENDPOINT_GET_PATH) as endpoint_get_mock:
+            httpretty.register_uri(httpretty.GET, AlgoliaEndpoint.URL,
+                                   responses=self._createPages(hits=[]),
+                                   content_type="application/json")
+
+            resp = list(CommentScraper().getComments(since=42))
+            self.assertEquals(endpoint_get_mock.call_args[0][0], "comment",
+                              "Correct tag was used")
+
+    @httpretty.activate
+    def test_get_comments(self):
+        hits = [CommentFactory(), CommentFactory()]
+
+        httpretty.register_uri(httpretty.GET, AlgoliaEndpoint.URL,
+                               responses=self._createPages(hits=hits),
+                               content_type="application/json")
+
+        resp = list(CommentScraper().getComments(since=42))
+        expected = Scraper._translateFields({ "hits": hits },
+                CommentScraper.FIELDS)
+        self.assertListEqual(resp, expected)
+
--- tests/utils.py
+++ tests/utils.py
@@ -12,6 +12,23 @@ class ItemFactory(factory.Factory):
     title = "Test item"
 
 
+class CommentFactory(factory.Factory):
+    FACTORY_FOR = dict
+
+    created_at = "2014-04-03T10:17:28.000Z"
+    title = "Test comment"
+    url = "www.google.com"
+    comment_text = "Fuzzy wuzzy was a bear"
+    story_id = 42
+    story_title = "Bear kills man"
+    story_url = "www.bing.com"
+    author = "yourmom"
+    points = 42
+    created_at_i = 42
+    objectID = 42
+    parent_id = 42
+
+
 class ResponseFactory(factory.Factory):
     FACTORY_FOR = dict
 
