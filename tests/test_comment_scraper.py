36b92c177bbc6a77bc0fc070af7b3e06453834b7
Andrei Picus <office.nightcrawler@gmail.com>
2014-04-04 11:26:17 +0300

    Rename mocked method path and variable.

--- tests/test_comment_scraper.py
+++ tests/test_comment_scraper.py
@@ -7,17 +7,17 @@ from .utils import BaseTest, CommentFactory
 
 
 class TestCommentScraper(BaseTest):
-    ENDPOINT_GET_PATH = "hackernews_scraper.hnscraper.Scraper.scrape"
+    SCRAPER_SCRAPE_PATH = "hackernews_scraper.hnscraper.Scraper.scrape"
 
     @httpretty.activate
     def test_correct_tag(self):
-        with patch(self.ENDPOINT_GET_PATH) as endpoint_get_mock:
+        with patch(self.SCRAPER_SCRAPE_PATH) as scraper_scrape_mock:
             httpretty.register_uri(httpretty.GET, AlgoliaEndpoint.URL,
                                    responses=self._createPages(hits=[]),
                                    content_type="application/json")
 
             list(CommentScraper().getComments(since=42))
-            self.assertEquals(endpoint_get_mock.call_args[0][0], "comment",
+            self.assertEquals(scraper_scrape_mock.call_args[0][0], "comment",
                               "Correct tag was used")
 
     @httpretty.activate
