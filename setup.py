d9638f49c1a88ced8b76a28a97df175843d2831f
Alex Palcuie <alex.palcuie@hootsuite.com>
2014-07-11 15:47:12 +0300

    Specify version by hand

--- setup.py
+++ setup.py
@@ -1,11 +1,9 @@
 from setuptools import setup, find_packages
 
-from hackernews_scraper import __version__
-
 setup(name='hackernews_scraper',
       description='Python library for retrieving comments and stories from HackerNews',
       packages=find_packages(),
-      version=__version__,
+      version="1.0.1",
       install_requires=['requests'],
       url='https://github.com/NiGhTTraX/hackernews-scraper',
       license='MIT',
