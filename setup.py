25a7a309d8179313ef40c527e85169d0d3a1d1bd
Alex Palcuie <alex.palcuie@hootsuite.com>
2014-07-11 15:54:13 +0300

    Add boschetar way for get_version #11

--- setup.py
+++ setup.py
@@ -1,9 +1,24 @@
 from setuptools import setup, find_packages
 
+import codecs
+import os
+import re
+
+def get_version(package_name):
+    version_re = re.compile(r"^__version__ = [\"']([\w_.-]+)[\"']$")
+    package_components = package_name.split('.')
+    init_path = os.path.join(root_dir, *(package_components + ['__init__.py']))
+    with codecs.open(init_path, 'r', 'utf-8') as f:
+        for line in f:
+            match = version_re.match(line[:-1])
+            if match:
+                return match.groups()[0]
+    return '0.1.0'
+
 setup(name='hackernews_scraper',
       description='Python library for retrieving comments and stories from HackerNews',
       packages=find_packages(),
-      version="1.0.1",
+      version=get_version(),
       install_requires=['requests'],
       url='https://github.com/NiGhTTraX/hackernews-scraper',
       license='MIT',
